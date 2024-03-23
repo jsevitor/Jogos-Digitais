4764f48695a7f8c9e081b3c8905d5d648bd6c77b
JOSE VITOR OLIVEIRA REIS <11956726675@pedag.local>
2024-03-22 22:14:59 -0300

    animation implemented

--- Jogo de Plataforma/scenes/Player.gd
+++ Jogo de Plataforma/scenes/Player.gd
@@ -1,10 +1,12 @@
 extends CharacterBody2D
 
 @onready var sprite = $Sprite2D
+@onready var animation = $AnimationPlayer
 
 var SPEED = 300.0
 var GRAVITY = 980
-var JUMP_VELOCITY = -480
+@export var JUMP_VELOCITY = -480
+@export var cloth_color: Color
 
 func _ready():
 	pass
@@ -14,6 +16,8 @@ func _process(delta):
 	velocity.y += GRAVITY * delta
 	
 	#Pulo
+	# is_action_just_pressed captura a tecla uma ÚNICA vez
+	# is_on_floor informa se o elemento está tocando o chão
 	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 	
@@ -21,5 +25,27 @@ func _process(delta):
 	var direction = Input.get_axis("move_left", "move_right")
 	velocity.x = direction * SPEED
 	
+	# movimenta o personagem de acordo com os valores do velocity
 	move_and_slide()
+	
+	# Virar/Espelhar o player
+	if direction < 0:
+		sprite.flip_h = true
+	elif direction > 0:
+		sprite.flip_h = false
+	
+	# Animações
+	if is_on_floor(): # Animações de chão
+		if velocity.x == 0:
+			animation.play("idle")
+		else:
+			animation.play("run")
+	else: #Animações de ar
+		if velocity.y < 0:
+			animation.play("jump")
+		else:
+			animation.play("fall") 
+		
+	
+	
 	
